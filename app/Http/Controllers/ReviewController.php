e091a7cd27925278fa4a0c4f2321e5ce4c7c4cc0
Ashikur Rahman <ashikurrahman7134@gmail.com>
2026-05-10 12:37:01 +0600

    product review store

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'ratting' => 'required|numeric|min:1|max:5',
+                'review' => 'required|string|max:1000',
+                'product_id' => 'required|integer|exists:products,id',
+                'image' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
+            ]);
+
+            $user  = auth()->user();
+
+            $review = new Review();
+            $review->user_id = $user->id;
+            $review->name = $user->name;
+            $review->phone = $user->phone;
+            $review->email = $user->email;
+            $review->ratting = $request->ratting;
+            $review->review = $request->review;
+            $review->product_id = $request->product_id;
+
+            // image upload
+            if ($request->hasFile('image')) {
+
+                $image = $request->file('image');
+                $imageName = time() . '-' . uniqid() . '.' . $image->getClientOriginalExtension();
+                $imagePath = 'public/uploads/review/';
+                $image->move($imagePath, $imageName);
+
+                $review->image = $imagePath . $imageName;
+            }
+
+            $review->status = 'pending';
+            $review->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Product Review added successfully',
+                'data' => $review,
+            ], 201);
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
