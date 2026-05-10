4ad79bee69400a1ab47311b4af4a93d712aadb5e
Ashikur Rahman <ashikurrahman7134@gmail.com>
2026-05-10 11:16:31 +0600

    model and migration ready for review

--- database/migrations/2026_05_10_045902_create_reviews_table.php
+++ database/migrations/2026_05_10_045902_create_reviews_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('product_id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone');
+            $table->string('rating');
+            $table->string('review');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
