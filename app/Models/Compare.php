d9c4b5e68ef9aa38650c4118187cee52a1f5487f
Ashikur Rahman <ashikurrahman7134@gmail.com>
2026-05-21 11:55:41 +0600

    copare model migration ready

---
+++ app/Models/Compare.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Compare extends Model
+{
+    protected $fillable = ['user_id', 'product_id'];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id')->select('id', 'name', 'slug', 'thumbnail_img');
+    }
+
+}
